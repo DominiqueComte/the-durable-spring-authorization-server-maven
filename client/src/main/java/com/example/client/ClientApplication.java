f9477e856d9f0dd3ef4606076ca915be88ba7877
Dom <dominique.comte.dev@gmail.com>
2023-12-17 15:18:57 +0100

    Follow IDE suggestions in simple client

--- client/src/main/java/com/example/client/ClientApplication.java
+++ client/src/main/java/com/example/client/ClientApplication.java
@@ -2,15 +2,13 @@ package com.example.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
 import java.util.Map;
 
-@Controller
-@ResponseBody
+@RestController
 @SpringBootApplication
 public class ClientApplication {
 
